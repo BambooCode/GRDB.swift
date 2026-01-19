a3b4171608cb530c08fdc83a6d03bcdc362d4c6a
Jaime Frutos <jaime@monkeytaps.app>
2026-01-19 11:22:19 +0100

    feat(): set watchos minimum version

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v11),
         .macOS(.v10_15),
+        .watchOS(.v7)
     ],
     products: [
         .library(name: "GRDB", targets: ["GRDB"]),
